4e21a2171c20f2563af9f6adb60a3efc984eb8a9
yustin <yustintroost@gmail.com>
2021-03-09 11:36:13 +0100

    Header

---
+++ JS/Main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#subnav").hide();
+	$('#Hamburger').click(function(){
+        $(this).toggleClass('open');
+        $("#subnav").slideToggle();
+    });
+    
+});
